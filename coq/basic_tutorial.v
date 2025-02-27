75d1b998ef42bc5b70a74f4ffade2ec494fb1e7f
Rubén Ballester <rballeba@gmail.com>
2025-02-27 15:41:42 +0100

    Adding some coq examples.

--- coq/basic_tutorial.v
+++ coq/basic_tutorial.v
@@ -105,3 +105,49 @@ Fixpoint sort l :=
     end.
 
     Compute sort (1::4::3::22::5::16::7::nil).
+
+Search True.
+
+SearchPattern ( _ <= _).
+
+(* Constructing new proofs *)
+
+Lemma example2: forall a b: Prop, a /\ b -> b /\ a.
+Proof.
+    intros a b H.
+    split.
+    destruct H as [H1 H2].
+    exact H2.
+    intuition.
+Qed.
+
+Lemma example3 : forall A B, A \/ B -> B \/ A.
+Proof.
+    intros A B H.
+    destruct H as [H1 | H2].
+    right; assumption.
+    left; assumption.
+Qed.
+
+Check le_n.
+
+Check le_S.
+
+Lemma example4: 3 <= 5.
+Proof.
+    apply le_S.
+    apply le_S.
+    apply le_n.
+Qed.
+
+Check Nat.le_trans.
+
+Lemma example5 : forall x y, x <= 10 -> 10 <= y -> x <= y.
+Proof.
+    intros x y x10 y10.
+    apply Nat.le_trans with (m:= 10).
+    assumption.
+    assumption.
+    
+
+
